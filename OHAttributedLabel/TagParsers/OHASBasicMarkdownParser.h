9f8aa53bfae214494c8e0c3adbf528f68b87079b
Emory Al-Imam <emorya@gmail.com>
2013-12-22 00:30:24 -0800

    Add a tag parser to support basic Markdown syntax (bold, italic, links, strikethrough, superscript, inline code).

---
+++ OHAttributedLabel/TagParsers/OHASBasicMarkdownParser.h
@@ -0,0 +1,10 @@
+//  OHASBasicMarkdownParser
+//
+//  Created by Emory Al-Imam on 12/21/13.
+//
+
+#import "OHASMarkupParserBase.h"
+
+@interface OHASBasicMarkdownParser : OHASMarkupParserBase
+
+@end
---
+++ OHAttributedLabel/TagParsers/OHASBasicMarkdownParser.m
@@ -0,0 +1,116 @@
+//  OHASBasicMarkdownParser
+//
+//  Created by Emory Al-Imam on 12/21/13.
+//
+
+#import "OHASBasicMarkdownParser.h"
+#import "NSAttributedString+Attributes.h"
+
+@implementation OHASBasicMarkdownParser
+
++(NSDictionary*)tagMappings
+{
+    return [NSDictionary dictionaryWithObjectsAndKeys:
+            
+            ^NSAttributedString*(NSAttributedString* str, NSTextCheckingResult* match)
+            {
+                NSRange textRange = [match rangeAtIndex:2];
+                if (textRange.length>0)
+                {
+                    NSMutableAttributedString* foundString = [[str attributedSubstringFromRange:textRange] mutableCopy];
+                    [foundString setTextBold:YES range:NSMakeRange(0,textRange.length)];
+                    return foundString;
+                } else {
+                    return nil;
+                }
+            }, @"(\\*{2}|_{2})(.+?)\\1", /* "**bold text**" "__bold text__" on word boundaries = xxx in bold */
+            
+            ^NSAttributedString*(NSAttributedString* str, NSTextCheckingResult* match)
+            {
+                NSRange textRange = [match rangeAtIndex:2];
+                if (textRange.length>0)
+                {
+                    NSMutableAttributedString* foundString = [[str attributedSubstringFromRange:textRange] mutableCopy];
+                    [foundString setTextItalics:YES range:NSMakeRange(0, foundString.length)];
+                    return foundString;
+                } else {
+                    return nil;
+                }
+            }, @"(?<!\\*|_)(\\*|_)([^*|_].+?)\\1(?!\\*|_)", /* "*italic text*" "_italic text_" on word boundaries = xxx in italic */
+            
+            ^NSAttributedString*(NSAttributedString* str, NSTextCheckingResult* match)
+            {
+                NSRange textRange = [match rangeAtIndex:2];
+                if (textRange.length>0)
+                {
+                    NSMutableAttributedString* foundString = [[str attributedSubstringFromRange:textRange] mutableCopy];
+                    [foundString setTextIsStrikethroughed:YES];
+                    return foundString;
+                } else {
+                    return nil;
+                }
+            }, @"(\\~~)(.+?)\\1", /* "~~strikethrough text~~" on word boundaries = xxx in strikethrough */
+            
+            ^NSAttributedString*(NSAttributedString* str, NSTextCheckingResult* match)
+            {
+                NSRange textRange = [match rangeAtIndex:2];
+                if (textRange.length>0)
+                {
+                    NSMutableAttributedString* foundString = [[str attributedSubstringFromRange:textRange] mutableCopy];
+                    [foundString setTextIsSuperscripted:YES];
+                    return foundString;
+                } else {
+                    return nil;
+                }
+            }, @"\\b(.+?)\\^(.+?)\\b", /* "superscript^text" */
+            
+            ^NSAttributedString*(NSAttributedString* str, NSTextCheckingResult* match)
+            {
+                NSRange textRange = [match rangeAtIndex:2];
+                if (textRange.length>0)
+                {
+                    NSMutableAttributedString* foundString = [[str attributedSubstringFromRange:textRange] mutableCopy];
+                    CTFontRef font = [str fontAtIndex:textRange.location effectiveRange:NULL];
+                    [foundString setFontName:@"Courier" size:CTFontGetSize(font)];
+                    return foundString;
+                } else {
+                    return nil;
+                }
+            }, @"(`)(.+?)\\1", /* "`xxx`" on word boundaries = xxx in Courier font */
+            
+            ^NSAttributedString*(NSAttributedString* str, NSTextCheckingResult* match)
+            {
+                NSRange colorRange = [match rangeAtIndex:1];
+                NSRange textRange = [match rangeAtIndex:2];
+                if ((colorRange.length>0) && (textRange.length>0))
+                {
+                    NSString* colorName = [str attributedSubstringFromRange:colorRange].string;
+                    UIColor* color = OHUIColorFromString(colorName);
+                    NSMutableAttributedString* foundString = [[str attributedSubstringFromRange:textRange] mutableCopy];
+                    [foundString setTextColor:color];
+                    return foundString;
+                } else {
+                    return nil;
+                }
+            }, @"\\{(.+?)\\|(.+?)\\}", /* "{color|text}" = text in specified color */
+            
+            ^NSAttributedString*(NSAttributedString* str, NSTextCheckingResult* match)
+            {
+                NSRange textRange = [match rangeAtIndex:1];
+                NSRange linkRange = [match rangeAtIndex:2];
+                if ((linkRange.length>0) && (textRange.length>0))
+                {
+                    NSString* linkString = [str attributedSubstringFromRange:linkRange].string;
+                    linkString = [linkString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+                    NSMutableAttributedString* foundString = [[str attributedSubstringFromRange:textRange] mutableCopy];
+                    [foundString setLink:[NSURL URLWithString:linkString] range:NSMakeRange(0, foundString.length)];
+                    return foundString;
+                } else {
+                    return nil;
+                }
+            }, @"\\b\\[(.+?)\\]\\((.+?)\\)\\b", /* "[text](link)" on word boundaries = add link to text */
+            
+            nil];
+}
+
+@end
